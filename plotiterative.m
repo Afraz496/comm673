620557b565c994da6afa924f41ff98e57394f222
Afraz Khan <afrazarif@hotmail.com>
2021-04-24 00:17:15 -0700

    Fixed output to not run into convergence issues

--- plotiterative.m
+++ plotiterative.m
@@ -151,7 +151,7 @@ ylabel('$\eta\mu^\eta $', 'Interpreter', 'latex');
 title('\bf{Drift of $\eta$}', 'Interpreter', 'latex')
 
 figure('Name','a_H','NumberTitle','off')
-for i = 1:3
+for i = 1:2
 %Call the iterative method on the i-th sigma value
     i
     [eta_grid(:,i), q_vec(:,i), sigma_tot_vec(:,i), eta_sigma_eta_vec(:,i), eta_mu_eta_vec(:,i)] = sannikov(sigma_vec(1), a_H_vec(i), chi_bar_vec(1), gamma_vec(1));
@@ -163,8 +163,6 @@ ax1 = nexttile;
 plot(ax1, eta_grid(:,1), q_vec(:,1));
 hold(ax1,'on')
 plot(ax1, eta_grid(:,2), q_vec(:,2));
-hold(ax1,'on')
-plot(ax1, eta_grid(:,3), q_vec(:,3));
 ylabel('$q$', 'Interpreter', 'latex');
 title('\bf{Price of capital good}', 'Interpreter', 'latex')
 
@@ -174,8 +172,6 @@ plot(ax2, eta_grid(:,1), sigma_tot_vec(:,1) - sigma_vec(:,1));
 hold(ax2,'on')
 plot(ax2, eta_grid(:,2), sigma_tot_vec(:,2) - sigma_vec(:,1));
 hold(ax2,'on')
-plot(ax2, eta_grid(:,3), sigma_tot_vec(:,3) - sigma_vec(:,1));
-hold(ax2,'on')
 ylabel('$\sigma^q$', 'Interpreter', 'latex');
 title('\bf{Price volatility}', 'Interpreter', 'latex')
 
@@ -184,8 +180,6 @@ ax3 = nexttile;
 plot(ax3, eta_grid(:,1),eta_sigma_eta_vec(:,1));
 hold(ax3, 'on')
 plot(ax3, eta_grid(:,2),eta_sigma_eta_vec(:,2));
-hold(ax3, 'on')
-plot(ax3, eta_grid(:,3),eta_sigma_eta_vec(:,3));
 hold(ax3, 'on')
 ylabel('$\eta\sigma^\eta$', 'Interpreter', 'latex');
 title('\bf{Volatility of $\eta$}', 'Interpreter', 'latex')
@@ -195,59 +189,6 @@ plot(ax4, eta_grid(:,1),eta_mu_eta_vec(:,1), eta_grid(:,1), zeros(N_eta,1), 'k:'
 hold(ax4,'on')
 plot(ax4, eta_grid(:,2),eta_mu_eta_vec(:,2),'r', eta_grid(:,2), zeros(N_eta,1), 'k:');
 hold(ax4,'on')
-plot(ax4, eta_grid(:,3),eta_mu_eta_vec(:,3),'y', eta_grid(:,3), zeros(N_eta,1), 'k:');
-hold(ax4,'on')
 ylabel('$\eta\mu^\eta $', 'Interpreter', 'latex');
 title('\bf{Drift of $\eta$}', 'Interpreter', 'latex')
 
-
-figure('Name','gamma','NumberTitle','off')
-for i = 1:3
-%Call the iterative method on the i-th sigma value
-    [eta_grid(:,i), q_vec(:,i), sigma_tot_vec(:,i), eta_sigma_eta_vec(:,i), eta_mu_eta_vec(:,i)] = sannikov(sigma_vec(1), a_H_vec(1), chi_bar_vec(1), gamma_vec(i));
-    show_plots(eta_grid(:,i), q_vec(:,i), sigma_tot_vec(:,i), sigma_vec(1), eta_sigma_eta_vec(:,i), eta_mu_eta_vec(:,i), N_eta)
-    hold on
-end
-
-%attempt to plot the i-th gamma value
-%Top left
-ax1 = nexttile;
-plot(ax1, eta_grid(:,1), q_vec(:,1));
-hold(ax1,'on')
-plot(ax1, eta_grid(:,2), q_vec(:,2));
-hold(ax1,'on')
-plot(ax1, eta_grid(:,3), q_vec(:,3));
-ylabel('$q$', 'Interpreter', 'latex');
-title('\bf{Price of capital good}', 'Interpreter', 'latex')
-
-
-ax2 = nexttile;
-plot(ax2, eta_grid(:,1), sigma_tot_vec(:,1) - sigma_vec(:,1));
-hold(ax2,'on')
-plot(ax2, eta_grid(:,2), sigma_tot_vec(:,2) - sigma_vec(:,1));
-hold(ax2,'on')
-plot(ax2, eta_grid(:,3), sigma_tot_vec(:,3) - sigma_vec(:,1));
-hold(ax2,'on')
-ylabel('$\sigma^q$', 'Interpreter', 'latex');
-title('\bf{Price volatility}', 'Interpreter', 'latex')
-
-
-ax3 = nexttile;
-plot(ax3, eta_grid(:,1),eta_sigma_eta_vec(:,1));
-hold(ax3, 'on')
-plot(ax3, eta_grid(:,2),eta_sigma_eta_vec(:,2));
-hold(ax3, 'on')
-plot(ax3, eta_grid(:,3),eta_sigma_eta_vec(:,3));
-hold(ax3, 'on')
-ylabel('$\eta\sigma^\eta$', 'Interpreter', 'latex');
-title('\bf{Volatility of $\eta$}', 'Interpreter', 'latex')
-
-ax4 = nexttile;
-plot(ax4, eta_grid(:,1),eta_mu_eta_vec(:,1), eta_grid(:,1), zeros(N_eta,1), 'k:');
-hold(ax4,'on')
-plot(ax4, eta_grid(:,2),eta_mu_eta_vec(:,2), eta_grid(:,2), zeros(N_eta,1), 'k:');
-hold(ax4,'on')
-plot(ax4, eta_grid(:,3),eta_mu_eta_vec(:,3), eta_grid(:,3), zeros(N_eta,1), 'k:');
-hold(ax4,'on')
-ylabel('$\eta\mu^\eta $', 'Interpreter', 'latex');
-title('\bf{Drift of $\eta$}', 'Interpreter', 'latex')
